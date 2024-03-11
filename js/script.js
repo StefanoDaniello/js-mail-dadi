097048487cfd9fca70b37ffb87736f4b3b60a1a9
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-11 15:43:48 +0100

    esercizio dei dadi

--- js/script.js
+++ js/script.js
@@ -5,24 +5,9 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
 
+const emButton = document.getElementById('emButton');
 
-/*
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-*/
-
-
-
-
-
-
-const button = document.getElementById('stbutton');
-
-button.addEventListener("click", function(){
+emButton.addEventListener("click", function(){
     let email = [
     'stefanodaniello0@gmail.com',
     'stefanodaniello1@gmail.com',
@@ -39,13 +24,40 @@ button.addEventListener("click", function(){
             found=true;
         }
     }
-    const result = document.getElementById('result');
+    const emResult = document.getElementById('emResult');
     if(found){
         console.log('la tua email e presente nel sistema');
-        result.innerHTML='la tua email e presente nel sistema'
+        emResult.innerHTML='la tua email e presente nel sistema'
     }else{
         console.log('la tua email non e presente nel sistema');
-        result.innerHTML='la tua email non e presente nel sistema'
+        emResult.innerHTML='la tua email non e presente nel sistema'
     }
 })
 
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
+
+
+let rnButton =document.getElementById('rnButton');
+
+rnButton.addEventListener("click", function(){
+    let randomNumber=[];
+    randomNumber.push(getRndInteger(1,6));
+    console.log(randomNumber);
+    let userNumber=document.getElementById('userNumber').value;
+    console.log(userNumber)
+    const rnResult = document.getElementById('rnResult');
+    if(userNumber <0 || userNumber >6 || userNumber === ''){
+        rnResult.innerHTML='errore inserisci un numero valido tra 0 e 6'
+    }else if(randomNumber>userNumber){
+    rnResult.innerHTML='hai perso'
+    }else{
+    rnResult.innerHTML='hai vinto'
+    }
+})
